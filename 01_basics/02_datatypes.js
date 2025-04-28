34be67f5f0156dcdbf34aaafcba040a84cc9a2ab
rupa-315 <151958647+rupa-315@users.noreply.github.com>
2025-04-28 09:20:36 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using Node.js, not browser 
+
+console.log(3 + 3) //code readability should be high
+
+console.log("Rupa")
+
+let name = "rupa"
+let age = 22
+let isLoggedIn = false
+
+// PRIMITIVE DATATYPES
+// number => 2 to power 53 ..range
+// bigint => used for larged int value
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "rupa")
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
