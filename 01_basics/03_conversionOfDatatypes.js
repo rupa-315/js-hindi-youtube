812addfc64901ec2c9c2c2bc5c3d3967008c186c
rupa-315 <151958647+rupa-315@users.noreply.github.com>
2025-04-29 07:42:46 +0000

    string to number conversion

--- 01_basics/03_conversionOfDatatypes.js
+++ 01_basics/03_conversionOfDatatypes.js
@@ -38,3 +38,40 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+// **************** Operations ****************
+
+let value = 5
+let negValue = -value
+//console.log(negValue)
+
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+*/
+
+let str1 = "hello"
+let str2 = " rupa"
+
+let str3 = str1 + str2
+// console.log(str3); 
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2")
+
+// console.log( (3 + 4) * 5 % 3);
+
+console.log(+true)
+console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2  // code readability is not good here
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
