335b7f74214bfbad3914f51250fc3fe4564e621a
rupa-315 <151958647+rupa-315@users.noreply.github.com>
2025-04-29 08:02:49 +0000

    datatype comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// IN MOST OF THE CASES WE AVOID THESE TYPE PF CONVERSION
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===
+
+console.log("2" === 2);
